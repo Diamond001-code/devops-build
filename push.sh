fc987de40d06ffaefebde6df97708ef1e44b4da8
Karthik0114 <karthiksivakumar95@gmail.com>
2024-11-30 19:45:27 +0530

    pushed code

--- push.sh
+++ push.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Set your Docker Hub username and repository name (if not already set in Jenkins environment)
-DOCKER_USERNAME="karthik kavi"
-DOCKER_PASSWORD="dckr_pat_bjmeR0hYBq8ZGhyqTZxwVRVMY_U"
-DOCKER_REPO="karthiksivakumar0114/dev"
-
-# Log in to Docker Hub
+# Log in to Docker Hub using environment variables from GitHub Actions
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-# Push the image to Docker Hub
+# Push the Docker image to Docker Hub
 docker push "$DOCKER_REPO:latest"
 
 # Logout from Docker Hub (optional but good practice)
