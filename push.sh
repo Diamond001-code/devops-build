9d474973c7410b86983b92f84628e58e4b5aef75
Karthik0114 <karthiksivakumar95@gmail.com>
2024-11-30 20:04:57 +0530

    updated push

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Log in to Docker Hub using environment variables
-echo "dckr_pat_bjmeR0hYBq8ZGhyqTZxwVRVMY_U" | docker login --username "karthiksivakumar0114" --password-stdin
+echo "dckr_pat_bjhsjC32LQTdF7CJj37Nl2b74iY" | docker login --username "karthiksivakumar0114" --password-stdin
 
 # Push the Docker image to Docker Hub
 docker push "karthiksivakumar0114/dev:latest"
