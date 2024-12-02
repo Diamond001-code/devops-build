adb77c24fdacef1f42d8408596001ef2c96f5eb4
Diamond001-code <karthikkavi0114@gmail.com>
2024-12-02 13:35:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
-docker login -u karthiksivakumar0114  -p dckr_pat_pMAsmOeEalehL_PdwSyBKwD2-7I
-
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-       sh 'chmod +x build.sh'
-       sh './build.sh'
-
-        docker tag test karthiksivakumar0114/dev
-        docker push karthiksivakumar0114/dev
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u karthiksivakumar0114 -p dckr_pat_pMAsmOeEalehL_PdwSyBKwD2-7I
+    docker tag test karthiksivakumar0114/dev
+    docker push karthiksivakumar0114/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
-        sh 'chmod +x build.sh'
-        sh './build.sh'
-
-        docker tag test karthiksivakumar0114/prod
-        docker push karthiksivakumar0114/prod
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u karthiksivakumar0114 -p dckr_pat_pMAsmOeEalehL_PdwSyBKwD2-7I
+    docker tag test karthiksivakumar0114/prod 
+    docker push karthiksivakumar0114/prod
 fi
