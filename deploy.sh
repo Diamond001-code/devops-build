ca7585b773b936f21d8683531c63f205914a7968
Diamond001-code <karthikkavi0114@gmail.com>
2024-12-02 13:31:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,10 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
         docker tag test karthiksivakumar0114/dev
         docker push karthiksivakumar0114/dev
 
-if [[ $GIT_BRANCH == "origin/main" ]]; then
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
         sh 'chmod +x build.sh'
         sh './build.sh'
 
         docker tag test karthiksivakumar0114/prod
         docker push karthiksivakumar0114/prod
-
-else
-        echo "failed"
 fi
